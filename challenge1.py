1603d4fe1cef7a00da548519a5b0c8a155eef4cd
abvss13 <abdullahi.abass@student.moringaschool.com>
2023-11-25 15:38:54 +0300

    challenge 1 completed

--- challenge1.py
+++ challenge1.py
@@ -1,4 +1,16 @@
 def convert_to_24_hour(hour, minute, period):
-     if period.lower() == "am":
+   
+    if period.lower() == "am":
         if hour == 12:
             hour = 0
+    else:
+       
+        if hour != 12:
+            hour += 12
+
+    
+    return f"{hour:02d}{minute:02d}"
+
+
+result = convert_to_24_hour(8, 30, 'am')
+print(result)  
