e06843ce77e1e14a1de3900382515efbbad420c3
abvss13 <abdullahi.abass@student.moringaschool.com>
2023-11-25 15:46:26 +0300

    challenge 2 done

--- challenge2.py
+++ challenge2.py
@@ -1,1 +1,9 @@
 def exactly_two_positive(a, b, c):
+    
+    positive_count = sum(1 for num in (a, b, c) if num > 0)
+    
+    return positive_count == 2
+
+
+result = exactly_two_positive(2, 4, -3)
+print(result)  
