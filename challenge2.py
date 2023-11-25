ef42820df2a60faa8fb20edb9c93fb169eb1f3a4
abvss13 <abdullahi.abass@student.moringaschool.com>
2023-11-25 15:50:51 +0300

    comments added

--- challenge2.py
+++ challenge2.py
@@ -1,9 +1,12 @@
+# Define a function to check if exactly two out of three numbers are positive
 def exactly_two_positive(a, b, c):
-    
+    # Count the number of positive numbers among a, b, and c
     positive_count = sum(1 for num in (a, b, c) if num > 0)
     
+    # Check if the count of positive numbers is exactly 2
     return positive_count == 2
 
-
+# Example usage: Check if exactly two out of 2, 4, and -3 are positive
 result = exactly_two_positive(2, 4, -3)
-print(result)  
+# Print the result
+print(result)
