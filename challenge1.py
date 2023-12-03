6d9c582c8f9556f5df62139e2eaa28d60d683fd2
abvss13 <abdullahi.abass@student.moringaschool.com>
2023-12-03 11:19:51 +0300

    done

--- challenge1.py
+++ challenge1.py
@@ -1,17 +1,17 @@
-def convert_to_24_hour(hour, minute, period):
-    # Check if it's morning or afternoon (AM or PM)
-    if period.lower() == "am":
-        # If it's morning and the hour is 12, change it to 0
-        if hour == 12:
-            hour = 0
-    else:
-        # If it's afternoon and not 12 PM, add 12 to the hour
-        if hour != 12:
-            hour += 12
+# Function to convert time from 12-hour format to 24-hour format
+def convert_to_24_hour(hour, minute, time):
+    # Check if the given time is in the evening ("pm") and not 12:00 pm
+    if time == "pm" and hour != 12:
+        # If yes, add 12 to the hour to convert to 24-hour format
+        hour += 12
+    # Check if the given time is in the morning ("am") and it's 12:00 am
+    elif time == "am" and hour == 12:
+        # If yes, set hour to 0 to convert to 24-hour format
+        hour = 0
+    
+    # Return the time in 24-hour format as a formatted string
+    return f"{hour:02}{minute:02}"
 
-    # Combine the hour and minute and return as a 24-hour time string
-    return f"{hour:02d}{minute:02d}"
-
-# Example: Convert 8:30 AM to 24-hour format
-result = convert_to_24_hour(8, 30, 'am')
-print(result)
+# Example usage of the function with 12:30 am and 12:30 pm
+print(convert_to_24_hour(12, 30, "am"))  
+print(convert_to_24_hour(12, 30, "pm"))
